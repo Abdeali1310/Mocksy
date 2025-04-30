528afa0a023ce117d5b6bed1be0ccd70f6a84fd5
unknown <abdealidahodwala163@gmail.com>
2025-04-30 20:58:21 +0530

    FEAT : Implement one-on-one interview session with VAPI AI Agent

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
-import { dummyInterviews } from "@/constants";
 import InterviewCard from "@/components/InterviewCard";
-const HomePage = () => {
+import { getCurrentUser } from "@/lib/actions/auth.action";
+import { getInterviewsByUserId, getLatestInterviews } from "@/lib/actions/general.action";
+const HomePage = async () => {
+  const user = await getCurrentUser();
+
+  const [userInterviews,latestInterviews] = await Promise.all([
+    await getInterviewsByUserId(user?.id),
+    await getLatestInterviews({userId:user?.id}),
+  ])
+  const hasPastInterviews = userInterviews?.length > 0;
+  const hasUpcomingInterviews = latestInterviews?.length > 0;
   return (
     <>
       <section className="card-cta">
@@ -32,11 +41,10 @@ const HomePage = () => {
         <h2>Your Interviews</h2>
 
         <div className="interviews-section">
-          {dummyInterviews.map((interview) => (
+          {hasPastInterviews ?  (userInterviews?.map((interview) => (
             <InterviewCard {...interview} key={interview.id} />
-          ))}
+          ))) : <p>You haven&apos;t taken any interviews yet</p>}
 
-          {/*<p>You haven&apos;t taken any interviews yet</p>*/}
         </div>
       </section>
 
@@ -44,9 +52,9 @@ const HomePage = () => {
         <h2>Take an Interview</h2>
 
         <div className="interviews-section">
-          {dummyInterviews.map((interview) => (
+        {hasUpcomingInterviews ?  (latestInterviews?.map((interview) => (
             <InterviewCard {...interview} key={interview.id} />
-          ))}
+          ))) : <p>There are no new interviews available</p>}
         </div>
       </section>
     </>
--- src/lib/actions/auth.action.ts
+++ src/lib/actions/auth.action.ts
@@ -79,38 +79,37 @@ export async function setSessionCookie(idToken: string) {
   });
 }
 
-
-
 export async function getCurrentUser(): Promise<User | null> {
-    const cookieStore = await cookies();
+  const cookieStore = await cookies();
 
-    const sessionCookie = cookieStore.get('session')?.value;
+  const sessionCookie = cookieStore.get("session")?.value;
 
-    if(!sessionCookie) return null;
+  if (!sessionCookie) return null;
 
-    try {
-        const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
 
-        const userRecord = await db.
-            collection('users')
-            .doc(decodedClaims.uid)
-            .get();
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
 
-        if(!userRecord.exists) return null;
+    if (!userRecord.exists) return null;
 
-        return {
-            ...userRecord.data(),
-            id: userRecord.id,
-        } as User;
-    } catch (e) {
-        console.log(e)
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (e) {
+    console.log(e);
 
-        return null;
-    }
+    return null;
+  }
 }
 
 export async function isAuthenticated() {
-    const user = await getCurrentUser();
+  const user = await getCurrentUser();
 
-    return !!user;
+  return !!user;
 }
+
